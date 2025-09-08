9b6fb363aa478acc5953de201b73d8cb06cfb9de
github-action-benchmark <github@users.noreply.github.com>
2025-09-08 11:25:27 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 9e79b53465fc9c5ea368a316e14e52161b45e529

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1756774387243,
+  "lastUpdate": 1757330727826,
   "repoUrl": "https://github.com/vnsavitri/n8n-mcp",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -104,6 +104,58 @@ window.BENCHMARK_DATA = {
             "unit": "ms",
             "range": 0.40779999999999994,
             "extra": "14962 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "56956555+czlonkowski@users.noreply.github.com",
+            "name": "Romuald Członkowski",
+            "username": "czlonkowski"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9e79b53465fc9c5ea368a316e14e52161b45e529",
+          "message": "Merge pull request #178 from amauryconstant/feature/add-readonly-isArchived-property\n\nAdd isArchived field to workflow responses and types",
+          "timestamp": "2025-09-04T15:42:20+02:00",
+          "tree_id": "f25f250fb33a322faeed9f0fc4abf58cbb6f6f5b",
+          "url": "https://github.com/vnsavitri/n8n-mcp/commit/9e79b53465fc9c5ea368a316e14e52161b45e529"
+        },
+        "date": 1757330727122,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0189,
+            "range": "0.4299",
+            "unit": "ms",
+            "extra": "52924 ops/sec"
+          },
+          {
+            "name": "sample - array sorting - large",
+            "value": 3.1696,
+            "range": "0.6916000000000002",
+            "unit": "ms",
+            "extra": "315 ops/sec"
+          },
+          {
+            "name": "sample - string concatenation",
+            "value": 0.0047,
+            "range": "0.27390000000000003",
+            "unit": "ms",
+            "extra": "213141 ops/sec"
+          },
+          {
+            "name": "sample - object creation",
+            "value": 0.0669,
+            "range": "0.356",
+            "unit": "ms",
+            "extra": "14953 ops/sec"
           }
         ]
       }
